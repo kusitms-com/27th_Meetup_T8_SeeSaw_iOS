bd2d3b95acd783d21736eef512d1701e0a1f6221
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-15 16:40:07 +0900

    Initial Commit

---
+++ SeeSaw/SeeSawApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeSawApp.swift
+//  SeeSaw
+//
+//  Created by 이안진 on 2023/04/15.
+//
+
+import SwiftUI
+
+@main
+struct SeeSawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
