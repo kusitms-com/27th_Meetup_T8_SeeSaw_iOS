3b480b2cc89d0a20126cf2e990a23851c898807e
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-21 17:24:36 +0900

    Chore(#2-Kakao Login): Kakao SDK 초기화 코드 추가

--- SeeSaw/SeeSawApp.swift
+++ SeeSaw/SeeSawApp.swift
@@ -5,13 +5,41 @@
 //  Created by 이안진 on 2023/04/15.
 //
 
+import KakaoSDKAuth
+import KakaoSDKCommon
 import SwiftUI
 
 @main
 struct SeeSawApp: App {
+    init() {
+        // Kakao SDK 초기화
+        let kakaoNativeAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        KakaoSDK.initSDK(appKey: kakaoNativeAppKey as! String)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+/*
+ import SwiftUI
+ import KakaoSDKCommon
+ import KakaoSDKAuth
+ ...
+
+ @main
+ struct SwiftUI_testApp: App {
+
+     ...
+     init() {
+         // Kakao SDK 초기화
+         KakaoSDK.initSDK(appKey: "NATIVE_APP_KEY")
+     }
+     ...
+
+ }
+ */
