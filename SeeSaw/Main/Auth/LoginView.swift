a33488f1d2b72f05b9ffa82970426be72717d55d
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-21 16:05:44 +0900

    Design: Sign in with Apple Button 생성

--- SeeSaw/Main/Auth/LoginView.swift
+++ SeeSaw/Main/Auth/LoginView.swift
@@ -5,11 +5,31 @@
 //  Created by 이안진 on 2023/04/21.
 //
 
+import AuthenticationServices
 import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Apple Login")
+            SignInWithAppleButton { request in
+                request.requestedScopes = [.email, .fullName]
+            } onCompletion: { result in
+                switch result {
+                case .success(let auth):
+                    switch auth.credential {
+                    case let credential as ASAuthorizationAppleIDCredential:
+                        print(credential.identityToken ?? "DEBUG: credential.identityToken is nil")
+                    default:
+                        print("DEBUG: sign success but credetial is nil")
+                        // break
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+            .frame(width: 280, height: 60)
+        }
     }
 }
 
--- SeeSaw/Main/Main.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Main.swift
-//  SeeSaw
-//
-//  Created by 이안진 on 2023/04/21.
-//
-
-import Foundation
