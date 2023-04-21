c19744c9295704773aa97c51dad28043abdac88d
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-21 15:57:58 +0900

    Add: LoginView 파일 생성

---
+++ SeeSaw/Main/Auth/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  SeeSaw
+//
+//  Created by 이안진 on 2023/04/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
