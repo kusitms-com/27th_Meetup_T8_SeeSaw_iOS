44b0b602dabe657fcaa4789175b1137da9cdf5a0
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-21 17:51:24 +0900

    Design(#2-Kakao Login): Kakao login, logout Button

--- SeeSaw/Main/Auth/LoginView.swift
+++ SeeSaw/Main/Auth/LoginView.swift
@@ -11,24 +11,49 @@ import SwiftUI
 struct LoginView: View {
     var body: some View {
         VStack {
-            Text("Apple Login")
-            SignInWithAppleButton { request in
-                request.requestedScopes = [.email, .fullName]
-            } onCompletion: { result in
-                switch result {
-                case .success(let auth):
-                    switch auth.credential {
-                    case let credential as ASAuthorizationAppleIDCredential:
-                        print(credential.identityToken ?? "DEBUG: credential.identityToken is nil")
-                    default:
-                        print("DEBUG: sign success but credetial is nil")
-                        // break
+            VStack {
+                Text("Apple Login")
+                SignInWithAppleButton { request in
+                    request.requestedScopes = [.email, .fullName]
+                } onCompletion: { result in
+                    switch result {
+                    case .success(let auth):
+                        switch auth.credential {
+                        case let credential as ASAuthorizationAppleIDCredential:
+                            print(credential.identityToken ?? "DEBUG: credential.identityToken is nil")
+                        default:
+                            print("DEBUG: sign success but credetial is nil")
+                            // break
+                        }
+                    case .failure(let error):
+                        print(error)
                     }
-                case .failure(let error):
-                    print(error)
+                }
+                .frame(width: 280, height: 60)
+            }
+            .padding(.bottom, 60)
+            
+            VStack {
+                Text("Kakao Login")
+                Button {
+                    
+                } label: {
+                    Image("KakaoLoginWideButton")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 280)
+                }
+                Button {
+                    // logout
+                } label: {
+                    Text("카카오 로그아웃")
+                        .frame(width: 280, height: 40)
+                        .foregroundColor(.black)
+                        .background(Color(red: 0.996, green: 0.898, blue: 0))
+                        .cornerRadius(6)
+                        .padding(.top, 10)
                 }
             }
-            .frame(width: 280, height: 60)
         }
     }
 }
