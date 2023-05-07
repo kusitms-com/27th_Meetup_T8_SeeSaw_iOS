39613e55636f12f8309074365c5425c2de03c026
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-07 17:26:08 +0900

    Add(gif-image): add GifImage View file

---
+++ SeeSaw/Service/GifImage/GifImage.swift
@@ -0,0 +1,20 @@
+//
+//  GifImage.swift
+//  SeeSaw
+//
+//  Created by 이안진 on 2023/05/07.
+//
+
+import SwiftUI
+
+struct GifImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GifImage_Previews: PreviewProvider {
+    static var previews: some View {
+        GifImage()
+    }
+}
