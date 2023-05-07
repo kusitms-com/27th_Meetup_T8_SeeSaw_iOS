bf05f8f4c22a9f50e5e9be46a9c13110e9a0dadf
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-07 17:39:25 +0900

    Feat(gif-image): WebKit을 이용한 Gif 변환 구현

--- SeeSaw/Service/GifImage/GifImage.swift
+++ SeeSaw/Service/GifImage/GifImage.swift
@@ -6,15 +6,38 @@
 //
 
 import SwiftUI
+import WebKit
 
-struct GifImage: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct GifImage: UIViewRepresentable {
+    private let name: String
+
+    init(_ name: String) {
+        self.name = name
     }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        let url = Bundle.main.url(forResource: name, withExtension: "gif")!
+        let data = (try? Data(contentsOf: url)) ?? Data()
+        webView.load(
+            data,
+            mimeType: "image/gif",
+            characterEncodingName: "UTF-8",
+            baseURL: url.deletingLastPathComponent()
+        )
+        webView.scrollView.isScrollEnabled = false
+
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.reload()
+    }
+
 }
 
 struct GifImage_Previews: PreviewProvider {
     static var previews: some View {
-        GifImage()
+        GifImage("LoginImage")
     }
 }
