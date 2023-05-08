d636924b7e08d337c35f523e1043a1ff84134598
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-09 00:17:26 +0900

    Feat(#22-auto-login): ContentView가 나타났을 때 refreshToken으로 regenerate 함수 호출

--- SeeSaw/ContentView.swift
+++ SeeSaw/ContentView.swift
@@ -5,16 +5,23 @@
 //  Created by 이안진 on 2023/04/15.
 //
 
+import KeychainSwift
 import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var authVM: AuthViewModel
+    let keychain = KeychainSwift()
     
     var body: some View {
-        if authVM.isLoggedIn ?? false {
-            SeeSawTabView()
-        } else {
-            LoginView()
+        VStack {
+            if authVM.isLoggedIn ?? false {
+                SeeSawTabView()
+            } else {
+                LoginView()
+            }
+        }
+        .onAppear {
+            authVM.regenerateToken(req: PostRegenerateTokenRequest(refreshToken: keychain.get("refreshToken") ?? ""))
         }
     }
 }
