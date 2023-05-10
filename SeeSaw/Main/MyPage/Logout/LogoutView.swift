3b000aca3b552320c2b477681ea69e0236c7c5f6
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-11 01:41:01 +0900

    Feat(#7-my-page): LogoutView 뒤 화면 어둡게 처리

--- SeeSaw/Main/MyPage/Logout/LogoutView.swift
+++ SeeSaw/Main/MyPage/Logout/LogoutView.swift
@@ -11,19 +11,25 @@ struct LogoutView: View {
     @Binding var showLogoutView: Bool
     
     var body: some View {
-        LargePopUpView(title: "로그아웃",
-                       isXmarkExist: false,
-                       isImageGIF: false,
-                       imageName: "LogoutImage",
-                       popUpMessage: "로그아웃하시겠어요?",
-                       firstButtonContext: "네",
-                       secondButtonContext: "아니오",
-                       firstButtonAction: logout,
-                       secondButtonAction: closePopUpView)
+        ZStack {
+            Color.black.opacity(0.2)
+                .ignoresSafeArea()
+            
+            LargePopUpView(title: "로그아웃",
+                           isXmarkExist: false,
+                           isImageGIF: false,
+                           imageName: "LogoutImage",
+                           popUpMessage: "로그아웃하시겠어요?",
+                           firstButtonContext: "네",
+                           secondButtonContext: "아니오",
+                           firstButtonAction: logout,
+                           secondButtonAction: closePopUpView)
+        }
     }
     
     func logout() {
-        
+        // TODO: logout
+        self.showLogoutView = false
     }
     
     func closePopUpView() {
