76dd04edf230423f49d776ff2e45d12ecb19af83
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2023-05-10 19:32:43 +0900

    Design(#13-ValueView): ValueYearView 추가 하여 디자인 변경

--- SeeSaw/Main/Value/ValueView.swift
+++ SeeSaw/Main/Value/ValueView.swift
@@ -1,4 +1,4 @@
-//
+///
 //  ValueView.swift
 //  SeeSaw
 //
@@ -8,39 +8,37 @@
 import SwiftUI
 
 struct ValueView: View {
-    var year: [String] = ["2020", "2021", "2022", "2023"]
+    var valuesExistingYear: [String] = ["2023", "2022", "2021", "2020"]
+    @State var valueYear: Int = 0
     var body: some View {
         VStack(alignment: .leading) {
-            ZStack {
-                Text("가치 설계 View")
-                Image("KakaoLoginButton")
+            VStack(alignment: .leading) {
+                Text("가치 설계도")
+                    .font(.ssHeading1)
+                Rectangle()
+                    .frame(width: 350, height: 185)
             }
             Rectangle()
                 .foregroundColor(.green)
                 .frame(width: 80, height: 5)
             Text("에몽님의")
-                .font(.system(size: 36))
-            Text("2023년의 가치는")
-                .font(.system(size: 36))
-            ValueButtonView(valueTitle: "도전", valueColor: .SeeSawRed)
-            ValueButtonView(valueTitle: "사랑", valueColor: .SeeSawYellow)
+                .font(.ssHeading1)
+            Text(valuesExistingYear[valueYear] + "년의 가치는")
+                .font(.ssHeading1)
+            ValueYearView(valueYear: self.valueYear)
             HStack {
-                ValueButtonView(valueTitle: "여유", valueColor: .SeeSawBlue)
-                Text("이에요")
-                    .font(.system(size: 36))
-            }
-            HStack {
-                ForEach(year, id: \.self, content: {(dataItem: String) in
+                ForEach(valuesExistingYear, id: \.self, content: {(dataItem: String) in
                     Button(dataItem) {
-                        
+                        valueYear = valuesExistingYear.firstIndex(of: dataItem) ?? 0
                     }
-                    .foregroundColor(.Gray600)
+                    .foregroundColor(valuesExistingYear[valueYear] == dataItem ? .SeeSawGreen : .Gray600)
                     .frame(width: 56, height: 25)
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(lineWidth: 1)
-                            .foregroundColor(.Gray600)
+                            .foregroundColor(valuesExistingYear[valueYear] == dataItem ? .SeeSawGreen : .Gray600)
                     )
+                    
                 })
             }
         }
