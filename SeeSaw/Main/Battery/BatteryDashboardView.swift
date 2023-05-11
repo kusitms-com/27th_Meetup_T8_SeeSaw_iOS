6ec71164c8493e53825f0b9292a57dc48ff1dd0f
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-11 12:58:23 +0900

    Rename(battery-view): 폴더와 View 이름을 Energy -> Battery로 변경

---
+++ SeeSaw/Main/Battery/BatteryDashboardView.swift
@@ -0,0 +1,20 @@
+//
+//  BatteryDashboardView.swift
+//  SeeSaw
+//
+//  Created by 이안진 on 2023/05/11.
+//
+
+import SwiftUI
+
+struct BatteryDashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BatteryDashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        BatteryDashboardView()
+    }
+}
