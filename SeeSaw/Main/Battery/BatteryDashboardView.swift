dd7b691ab48edfa72fe88095ce233e6e8b742470
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-12 16:34:44 +0900

    Feat(#30-battery-view): NavigationView 추가

--- SeeSaw/Main/Battery/BatteryDashboardView.swift
+++ SeeSaw/Main/Battery/BatteryDashboardView.swift
@@ -11,138 +11,150 @@ struct BatteryDashboardView: View {
     @State var progress: Double = 0.8
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .center, spacing: 0) {
-                VStack(alignment: .leading, spacing: 0) {
-                    HStack {
-                        Text("SeeSaw")
-                        Spacer()
-                        Image(systemName: "person.crop.circle")
-                    }
-                    .font(.system(size: 28))
-                    .padding(.bottom, 28)
-                    
-                    VStack(alignment: .leading) {
-                        Text("에몽님의")
-                        Text("에너지 배터리")
-                    }
-                    .font(.ssHeading1)
-                    
-                    Button {
-                        progress = 0.5
-                    } label: {
-                        Image(systemName: "questionmark.circle")
-                            .font(.system(size: 28))
-                            .foregroundColor(.Gray400)
-                            .padding(.vertical, 8)
-                    }
-                }
-                .padding(.horizontal, 20)
-                
-                ZStack(alignment: .center) {
-                    Circle()
-                        .foregroundColor(.Gray100)
-                        .frame(width: 380, height: 380)
-                    
-                    Circle()
-                        .stroke( // 1
-                            Color.SeeSawGreen.opacity(0.3),
-                            lineWidth: 30
-                        )
-                        .frame(width: 300, height: 300)
-                    
-                    Circle()
-                        .trim(from: 0, to: progress) // 1
-                        .stroke(
-                            Color.SeeSawGreen,
-                            style: StrokeStyle(
-                                lineWidth: 30,
-                                lineCap: .round
-                            )
-                        )
-                        .frame(width: 300, height: 300)
-                        .rotationEffect(.degrees(-90))
-                        .animation(.easeOut, value: progress)
-                    
-                    Text("%")
-                        .font(.ssHeading1)
-                        .offset(x: 72, y: -40)
-                    
-                    Text("80")
-                        .font(.system(size: 100))
-                        .fontWeight(.semibold)
-                }
-                
-                ZStack {
-                    Rectangle()
-                        .cornerRadius(200,
-                                      corners: .allCorners)
-                        .foregroundColor(.Gray100)
-                        .frame(width: 380, height: 200)
-                    
-                    VStack {
-                        Text("고속충전")
-                            .font(.ssHeading2)
-                            .padding(4)
-                        Text("아직 고속충전을 하지 않았어요")
-                        Text("고속충전을 하러 가볼까요")
-                        CapsuleButtonView(color: .Gray900,
-                                          text: "고속충전하기",
-                                          size: .small)
-                    }
-                }
-                
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("활동량")
-                                .font(.ssHeading2)
-                            ZStack(alignment: .bottom) {
-                                Rectangle()
-                                    .cornerRadius(10)
-                                    .foregroundColor(.Gray200)
-                                    .frame(height: 260)
-                                VStack {
-                                    Text("활동량 목표")
-                                        .font(.ssBlackBody1)
-                                        .foregroundColor(.Gray500)
-                                    CapsuleButtonView(color: .Gray900,
-                                                      text: "설정하기",
-                                                      size: .small)
-                                }
-                                .padding(.bottom, 12)
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .center, spacing: 0) {
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack {
+                            Text("SeeSaw")
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "person.crop.circle")
                             }
                         }
-                        .padding(.horizontal, 8)
+                        .font(.system(size: 28))
+                        .foregroundColor(.Gray400)
+                        .padding(.bottom, 28)
                         
                         VStack(alignment: .leading) {
-                            Text("수면")
-                                .font(.ssHeading2)
-                            ZStack(alignment: .bottom) {
-                                Rectangle()
-                                    .cornerRadius(10)
-                                    .foregroundColor(.Gray200)
-                                    .frame(height: 260)
-                                VStack {
-                                    Text("수면시간 목표")
-                                        .font(.ssBlackBody1)
-                                        .foregroundColor(.Gray500)
-                                    CapsuleButtonView(color: .Gray900,
-                                                      text: "설정하기",
-                                                      size: .small)
-                                }
-                                .padding(.bottom, 12)
-                            }
+                            Text("에몽님의")
+                            Text("에너지 배터리")
                         }
-                        .padding(.horizontal, 8)
+                        .font(.ssHeading1)
+                        
+                        Button {
+                            progress = 0.5
+                        } label: {
+                            Image(systemName: "questionmark.circle")
+                                .font(.system(size: 28))
+                                .foregroundColor(.Gray400)
+                                .padding(.vertical, 8)
+                        }
                     }
-                    .padding(12)
-                    .padding(.bottom, 60)
+                    .padding(.horizontal, 20)
+                    
+                    // 배터리
+                    ZStack(alignment: .center) {
+                        Circle()
+                            .foregroundColor(.Gray100)
+                            .frame(width: 380, height: 380)
+                        
+                        Circle()
+                            .stroke( // 1
+                                Color.SeeSawGreen.opacity(0.3),
+                                lineWidth: 30
+                            )
+                            .frame(width: 300, height: 300)
+                        
+                        Circle()
+                            .trim(from: 0, to: progress) // 1
+                            .stroke(
+                                Color.SeeSawGreen,
+                                style: StrokeStyle(
+                                    lineWidth: 30,
+                                    lineCap: .round
+                                )
+                            )
+                            .frame(width: 300, height: 300)
+                            .rotationEffect(.degrees(-90))
+                            .animation(.easeOut, value: progress)
+                        
+                        Text("%")
+                            .font(.ssHeading1)
+                            .offset(x: 72, y: -40)
+                        
+                        Text("80")
+                            .font(.system(size: 100))
+                            .fontWeight(.semibold)
+                    }
+                    
+                    // 고속충전
+                    ZStack {
+                        Rectangle()
+                            .cornerRadius(200,
+                                          corners: .allCorners)
+                            .foregroundColor(.Gray100)
+                            .frame(width: 380, height: 200)
+                        
+                        VStack {
+                            Text("고속충전")
+                                .font(.ssHeading2)
+                                .padding(4)
+                            Text("아직 고속충전을 하지 않았어요")
+                            Text("고속충전을 하러 가볼까요")
+                            CapsuleButtonView(color: .Gray900,
+                                              text: "고속충전하기",
+                                              size: .small)
+                        }
+                    }
+                    
+                    // 활동량, 수면
+                    VStack {
+                        HStack {
+                            // 활동량
+                            VStack(alignment: .leading) {
+                                Text("활동량")
+                                    .font(.ssHeading2)
+                                ZStack(alignment: .bottom) {
+                                    Rectangle()
+                                        .cornerRadius(10)
+                                        .foregroundColor(.Gray200)
+                                        .frame(height: 260)
+                                    VStack {
+                                        Text("활동량 목표")
+                                            .font(.ssBlackBody1)
+                                            .foregroundColor(.Gray500)
+                                        CapsuleButtonView(color: .Gray900,
+                                                          text: "설정하기",
+                                                          size: .small)
+                                    }
+                                    .padding(.bottom, 12)
+                                }
+                            }
+                            .padding(.horizontal, 8)
+                            
+                            // 수면
+                            VStack(alignment: .leading) {
+                                Text("수면")
+                                    .font(.ssHeading2)
+                                ZStack(alignment: .bottom) {
+                                    Rectangle()
+                                        .cornerRadius(10)
+                                        .foregroundColor(.Gray200)
+                                        .frame(height: 260)
+                                    VStack {
+                                        Text("수면시간 목표")
+                                            .font(.ssBlackBody1)
+                                            .foregroundColor(.Gray500)
+                                        CapsuleButtonView(color: .Gray900,
+                                                          text: "설정하기",
+                                                          size: .small)
+                                    }
+                                    .padding(.bottom, 12)
+                                }
+                            }
+                            .padding(.horizontal, 8)
+                        }
+                        .padding(12)
+                        .padding(.bottom, 60)
+                    }
+                    .background(Color.Gray100)
                 }
-                .background(Color.Gray100)
             }
+            .background(Color.Gray200)
         }
-        .background(Color.Gray200)
     }
 }
 
