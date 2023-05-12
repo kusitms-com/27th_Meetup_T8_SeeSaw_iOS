812ecd267baef41b3147d0187ef82b725a87ead3
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-12 14:21:35 +0900

    Style(#2-kakao-login): AuthViewModel 안쓰는 주석 제거

--- SeeSaw/Main/Auth/Login/AuthViewModel.swift
+++ SeeSaw/Main/Auth/Login/AuthViewModel.swift
@@ -63,39 +63,3 @@ class AuthViewModel: ObservableObject {
             }
     }
 }
-
-/*
- func login(req: PostLoginRequest) {
-     let url = "http://ec2-3-36-172-10.ap-northeast-2.compute.amazonaws.com/auth/login"
-     let parameters: [String: Any] = [
-         "id_token": req.idToken,
-         "access_token": req.accessToken,
-         "refresh_token": req.refreshToken,
-         "provider": req.provider
-     ]
-     let headers: HTTPHeaders = [
-         "Content-Type": "application/json"
-     ]
-     
-     AF.request(url,
-                method: .post,
-                parameters: parameters,
-                encoding: JSONEncoding.default,
-                headers: headers)
-         .responseDecodable(of: PostLoginResponse.self) { response in
-             switch response.result {
-             case .success(let response):
-                 self.keychain.set(response.accessToken,
-                                   forKey: "accessToken",
-                                   withAccess: .accessibleWhenUnlocked)
-                 self.keychain.set(response.refreshToken,
-                                   forKey: "refreshToken",
-                                   withAccess: .accessibleWhenUnlocked)
-                 print("accessToken \(response.accessToken)")
-                 print("refreshToken \(response.refreshToken)")
-             case .failure(let error):
-                 print(error)
-             }
-         }
- }
- */
