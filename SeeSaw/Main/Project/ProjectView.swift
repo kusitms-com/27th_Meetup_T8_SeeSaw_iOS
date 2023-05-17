4fc56a1552fb4dddfea2977945420c6c3d9a169b
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2023-05-17 10:03:01 +0900

    Feat(#27-ProjectVIew): ProjectView 구현

--- SeeSaw/Main/Project/ProjectView.swift
+++ SeeSaw/Main/Project/ProjectView.swift
@@ -2,14 +2,126 @@
 //  ProjectView.swift
 //  SeeSaw
 //
-//  Created by 이안진 on 2023/04/26.
+//  Created by 정태우 on 2023/04/26.
 //
 
 import SwiftUI
 
 struct ProjectView: View {
+    @State var isProgress = true
+    @State var progressNum: Int = 0
+    @State var completeNum: Int = 0
     var body: some View {
-        Text("Project View")
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Image("SeeSawLogoImage")
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "person.crop.circle")
+                            .resizable()
+                            .frame(width: 28, height: 28)
+                            .foregroundColor(.Gray400)
+                    }
+                }
+                .padding(.leading, 20)
+                .padding(.trailing, 28)
+                Spacer()
+                    .frame(height: 30)
+                VStack(alignment: .leading) {
+                    Text("에몽님의")
+                        .font(.ssHeading1)
+                        .foregroundColor(.GrayBlack)
+                    Text("프로젝트 기록")
+                        .font(.ssHeading1)
+                        .foregroundColor(.GrayBlack)
+                }
+                .padding(.leading, 20)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundColor(.Gray100)
+                        .frame(width: 350, height: 100)
+                        .overlay(
+                            Image("CloverImage")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 150, height: 250)
+                                .offset(x: 100, y: -30)
+                                .rotationEffect(Angle(degrees: -10))
+                        )
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("오늘의 회고 질문")
+                                .font(.ssBlackBody3)
+                                .foregroundColor(.Gray600)
+                            Text("팀원들과의 협업은")
+                                .font(.ssBlackTitle2)
+                                .foregroundColor(.Gray800)
+                            Text("잘 진행되고 있으신가요?")
+                                .font(.ssBlackTitle2)
+                                .foregroundColor(.Gray800)
+                        }
+                        Spacer()
+                    }
+                    .padding(.leading, 20)
+                }
+                VStack {
+                    Spacer()
+                        .frame(height: 30)
+                    HStack(spacing: 30) {
+                        Button {
+                            withAnimation {
+                                isProgress = true
+                            }
+                        } label: {
+                            Text("진행 중(\(progressNum))")
+                                .font(.ssWhiteBody1)
+                                .foregroundColor(isProgress ? .Gray900 : .Gray500)
+                        }
+                        Spacer()
+                            .frame(width: 50)
+                        Button {
+                            withAnimation {
+                                isProgress = false
+                            }
+                        } label: {
+                            Text("완료(\(completeNum))")
+                                .font(.ssWhiteBody1)
+                                .foregroundColor(isProgress ? .Gray500 : .Gray900)
+                        }
+                    }
+                    ZStack {
+                        Rectangle()
+                            .frame(height: 1)
+                            .foregroundColor(.Gray300)
+                        if isProgress {
+                            HStack {
+                                Rectangle()
+                                    .frame(width: 100, height: 2)
+                                    .foregroundColor(.GrayBlack)
+                                Spacer()
+                                    .frame(width: 165)
+                            }
+                        } else {
+                            HStack {
+                                Spacer()
+                                    .frame(width: 165)
+                                Rectangle()
+                                    .frame(width: 100, height: 2)
+                                    .foregroundColor(.GrayBlack)
+                            }
+                        }
+                    }
+                    if isProgress {
+                        ProgressProjectView()
+                    } else {
+                        CompleteProjectView()
+                    }
+                }
+            }
+        }
     }
 }
 
