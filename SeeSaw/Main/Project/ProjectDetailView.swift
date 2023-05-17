d89ee184ea5e72bad2491191dee05af39e7f299c
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2023-05-17 17:09:56 +0900

    Add(#27-ProjectView): ProjectDetailView 파일 추가

---
+++ SeeSaw/Main/Project/ProjectDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectDetailView.swift
+//  SeeSaw
+//
+//  Created by 정태우 on 2023/05/16.
+//
+
+import SwiftUI
+
+struct ProjectDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectDetailView()
+    }
+}
