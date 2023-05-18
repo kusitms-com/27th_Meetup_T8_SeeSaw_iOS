2a20927d3475177342c68d9a01b18f9cc7a7719e
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-19 05:32:21 +0900

    Style(#30-battery-view): right half circle 파일 분리

--- SeeSaw/Extensions/Circle/RightHalfCircle.swift
+++ SeeSaw/Extensions/Circle/RightHalfCircle.swift
@@ -1,21 +1,11 @@
 //
-//  HalfCircle.swift
+//  RightHalfCircle.swift
 //  SeeSaw
 //
 //  Created by 이안진 on 2023/05/19.
 //
 
 import SwiftUI
-
-struct HalfCircle: Shape {
-    func path(in rect: CGRect) -> Path {
-        let radius = rect.width / 2
-        let center = CGPoint(x: rect.midX, y: 0)
-        var path = Path()
-        path.addArc(center: center, radius: radius, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 0), clockwise: true)
-        return path
-    }
-}
 
 struct RightHalfCircle: Shape {
     var plusRatio: Double
