b2928f984d64adf8c3a873b7086ee403a30178d2
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-18 17:53:34 +0900

    Feat(onboarding): 가치 설명 화면에서 닉네임 표시

--- SeeSaw/Main/Auth/SelectValuesOnBoarding/Views/ExplainValuesView.swift
+++ SeeSaw/Main/Auth/SelectValuesOnBoarding/Views/ExplainValuesView.swift
@@ -15,9 +15,12 @@ enum ValuesMessages {
 시소는 바쁜 일상 속에서도
 나만의 가치를 잃지 않도록 도와줘요.
 """
-    static let second = """
+    static let secondFront = """
 중요한 3가지 가치를 설정해요.
-시소는 가치를 향한 00님의 여정을 함께해요!
+시소는 가치를 향한
+"""
+    static let secondBack = """
+님의 여정을 함께해요!
 
 일상에서 가치를 실천하면
 삶에 필요한 에너지가 충전돼요.
@@ -32,6 +35,7 @@ enum ValuesMessages {
 }
 
 struct ExplainValuesView: View {
+    @AppStorage("nickname") var nickname = ""
     @State private var selectedView = 1
     
     var body: some View {
@@ -41,8 +45,10 @@ struct ExplainValuesView: View {
             TabView(selection: $selectedView) {
                 ExplainValue(text: ValuesMessages.first, color: .SeeSawGreen)
                     .tag(1)
-                ExplainValue(text: ValuesMessages.second, color: .SeeSawBlue)
+                
+                ExplainValue(text: "\(ValuesMessages.secondFront) \(nickname)\(ValuesMessages.secondBack)", color: .SeeSawBlue)
                     .tag(2)
+                
                 ExplainValue(text: ValuesMessages.third, color: .SeeSawGreen)
                     .tag(3)
             }
