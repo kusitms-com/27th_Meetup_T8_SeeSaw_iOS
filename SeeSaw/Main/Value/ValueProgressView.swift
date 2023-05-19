4647efe5230411e02244646b81c3a6ca672c0c6e
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2023-05-19 10:00:34 +0900

    Feat(#13-ValueView): valueProjectView() 삽입

--- SeeSaw/Main/Value/ValueProgressView.swift
+++ SeeSaw/Main/Value/ValueProgressView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ValueProgressView: View {
     var value: Double
+    var backColor: Color
+    var foreColor: Color
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 RoundedRectangle(cornerRadius: 30)
                     .frame(width: geometry.size.width, height: geometry.size.height)
-                    .foregroundColor(.Gray300)
-                
+                    .foregroundColor(backColor)
                 RoundedRectangle(cornerRadius: 30)
                     .frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color.black)
+                    .foregroundColor(foreColor)
             }
         }
     }
@@ -26,6 +27,6 @@ struct ValueProgressView: View {
 
 struct ValueProgressView_Previews: PreviewProvider {
     static var previews: some View {
-        ValueProgressView(value: 0.7)
+        ValueProgressView(value: 0.7, backColor: .Gray300, foreColor: .black)
     }
 }
