88757cfdfe076992478d2d78e8225ebfa47d2cdd
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-19 19:58:32 +0900

    Design(#30-battery-view): 배터리 수준 차트 UI 구현

--- SeeSaw/Main/Battery/Views/BatteryHistoryView.swift
+++ SeeSaw/Main/Battery/Views/BatteryHistoryView.swift
@@ -10,54 +10,7 @@ import SwiftUI
 struct BatteryHistoryView: View {
     var body: some View {
         VStack {
-            VStack(alignment: .leading, spacing: 0) {
-                Text("배터리 수준")
-                    .font(.ssHeading2)
-                    .foregroundColor(.Gray900)
-                    .padding(.bottom, 12)
-                
-                ZStack(alignment: .bottom) {
-                    VStack(spacing: 0) {
-                        dashedLine
-                        dashedLine
-                            .padding(.vertical, 72)
-                        dashedLine
-                    }
-                    
-                    HStack(alignment: .bottom, spacing: 24) {
-                        Rectangle()
-                            .cornerRadius(90, corners: [.topLeft, .topRight])
-                            .foregroundColor(.SeeSawYellow)
-                            .frame(width: 20, height: 120)
-                        
-                        Rectangle()
-                            .cornerRadius(90, corners: [.topLeft, .topRight])
-                            .foregroundColor(.SeeSawYellow)
-                            .frame(width: 20, height: 72)
-                        Rectangle()
-                            .cornerRadius(90, corners: [.topLeft, .topRight])
-                            .foregroundColor(.SeeSawYellow)
-                            .frame(width: 20, height: 92)
-                        Rectangle()
-                            .cornerRadius(90, corners: [.topLeft, .topRight])
-                            .foregroundColor(.SeeSawYellow)
-                            .frame(width: 20, height: 138)
-                        Rectangle()
-                            .cornerRadius(90, corners: [.topLeft, .topRight])
-                            .foregroundColor(.SeeSawYellow)
-                            .frame(width: 20, height: 48)
-                        Rectangle()
-                            .cornerRadius(90, corners: [.topLeft, .topRight])
-                            .foregroundColor(.SeeSawYellow)
-                            .frame(width: 20, height: 125)
-                        Rectangle()
-                            .cornerRadius(90, corners: [.topLeft, .topRight])
-                            .foregroundColor(.SeeSawYellow)
-                            .frame(width: 20, height: 72)
-                    }
-                }
-            }
-            .padding(20)
+            BatteryVariationView()
             
             Divider()
             
@@ -234,6 +187,55 @@ struct BatteryHistoryView: View {
         .navigationBarTitleDisplayMode(.inline)
         .background(Color.Gray200)
     }
+}
+
+struct BatteryVariationView: View {
+    let data: [BatteryHistory] = [
+        BatteryHistory(date: "2023-05-16", batteryPercentage: 80),
+        BatteryHistory(date: "2023-05-15", batteryPercentage: 30),
+        BatteryHistory(date: "2023-05-14", batteryPercentage: 95),
+        BatteryHistory(date: "2023-05-13", batteryPercentage: 85),
+        BatteryHistory(date: "2023-05-12", batteryPercentage: 40),
+        BatteryHistory(date: "2023-05-11", batteryPercentage: 90),
+        BatteryHistory(date: "2023-05-10", batteryPercentage: 65)]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("배터리 수준")
+                .font(.ssHeading2)
+                .foregroundColor(.Gray900)
+                .padding(.bottom, 12)
+            
+            ZStack(alignment: .bottom) {
+                VStack(spacing: 0) {
+                    dashedLine
+                    dashedLine
+                        .padding(.vertical, 72)
+                    dashedLine
+                }
+                
+                HStack(alignment: .bottom, spacing: 16) {
+                    ForEach(data.indices, id: \.self) { index in
+                        let rev = 6 - index
+                        let height = CGFloat((148 * data[rev].batteryPercentage)) / 100
+                        
+                        VStack {
+                            Rectangle()
+                                .cornerRadius(90, corners: [.topLeft, .topRight])
+                                .foregroundColor(.SeeSawYellow)
+                                .frame(width: 20, height: height)
+                            
+                            Text(data[rev].date.suffix(5))
+                                .font(.ssBlackBody4)
+                                .foregroundColor(.Gray400)
+                        }
+                        .offset(y: 18)
+                    }
+                }
+            }
+        }
+        .padding(20)
+    }
     
     var dashedLine: some View {
         Line()
@@ -243,7 +245,10 @@ struct BatteryHistoryView: View {
     }
 }
 
-
+struct BatteryHistory {
+    let date: String
+    let batteryPercentage: Int
+}
 
 struct BatteryHistoryView_Previews: PreviewProvider {
     static var previews: some View {
