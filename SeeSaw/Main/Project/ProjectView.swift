b8a4a4bc8a8b9d7a8bdbe4e4c0839616a8735385
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-22 18:23:30 +0900

    Feat(toolbar): 상단 로고 및 마이페이지 툴바 구현

--- SeeSaw/Main/Project/ProjectView.swift
+++ SeeSaw/Main/Project/ProjectView.swift
@@ -11,117 +11,108 @@ struct ProjectView: View {
     @State var isProgress = true
     @State var progressNum: Int = 0
     @State var completeNum: Int = 0
+    
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                HStack {
-                    Image("SeeSawLogoImage")
-                    Spacer()
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "person.crop.circle")
-                            .resizable()
-                            .frame(width: 28, height: 28)
-                            .foregroundColor(.Gray400)
-                    }
-                }
-                .padding(.leading, 20)
-                .padding(.trailing, 28)
-                Spacer()
-                    .frame(height: 30)
+            VStack(spacing: 0) {
+                MainToolBar(feature: .projectLog)
+                
                 VStack(alignment: .leading) {
-                    Text("에몽님의")
-                        .font(.ssHeading1)
-                        .foregroundColor(.GrayBlack)
-                    Text("프로젝트 회고")
-                        .font(.ssHeading1)
-                        .foregroundColor(.GrayBlack)
-                }
-                .padding(.leading, 20)
-                ZStack {
-                    RoundedRectangle(cornerRadius: 12)
-                        .foregroundColor(.Gray100)
-                        .frame(width: 350, height: 100)
-                        .overlay(
-                            Image("CloverImage")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 150, height: 250)
-                                .offset(x: 100, y: -30)
-                                .rotationEffect(Angle(degrees: -10))
-                        )
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("오늘의 회고 질문")
-                                .font(.ssBlackBody2)
-                                .foregroundColor(.Gray600)
-                                .padding(.bottom, 4)
-                                
-                            Text("팀원들과의 협업은")
-                                .font(.ssBlackTitle2)
-                                .foregroundColor(.Gray800)
-                            Text("잘 진행되고 있으신가요?")
-                                .font(.ssBlackTitle2)
-                                .foregroundColor(.Gray800)
-                        }                        .padding(.horizontal, 10)
-                        Spacer()
+                    VStack(alignment: .leading) {
+                        Text("에몽님의")
+                            .font(.ssHeading1)
+                            .foregroundColor(.GrayBlack)
+                            .padding(.top, 10)
+                        Text("프로젝트 회고")
+                            .font(.ssHeading1)
+                            .foregroundColor(.GrayBlack)
                     }
                     .padding(.leading, 20)
-                }
-                VStack {
-                    Spacer()
-                        .frame(height: 30)
-                    HStack(spacing: 30) {
-                        Button {
-                            withAnimation {
-                                isProgress = true
-                            }
-                        } label: {
-                            Text("진행 중(\(progressNum))")
-                                .font(.ssWhiteBody1)
-                                .foregroundColor(isProgress ? .Gray900 : .Gray500)
-                        }
-                        Spacer()
-                            .frame(width: 50)
-                        Button {
-                            withAnimation {
-                                isProgress = false
-                            }
-                        } label: {
-                            Text("완료(\(completeNum))")
-                                .font(.ssWhiteBody1)
-                                .foregroundColor(isProgress ? .Gray500 : .Gray900)
-                        }
-                    }
                     ZStack {
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.Gray300)
-                        if isProgress {
-                            HStack {
-                                Rectangle()
-                                    .frame(width: 100, height: 2)
-                                    .foregroundColor(.GrayBlack)
-                                Spacer()
-                                    .frame(width: 165)
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundColor(.Gray100)
+                            .frame(width: 350, height: 100)
+                            .overlay(
+                                Image("CloverImage")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 150, height: 250)
+                                    .offset(x: 100, y: -30)
+                                    .rotationEffect(Angle(degrees: -10))
+                            )
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("오늘의 회고 질문")
+                                    .font(.ssBlackBody2)
+                                    .foregroundColor(.Gray600)
+                                    .padding(.bottom, 4)
+                                
+                                Text("팀원들과의 협업은")
+                                    .font(.ssBlackTitle2)
+                                    .foregroundColor(.Gray800)
+                                Text("잘 진행되고 있으신가요?")
+                                    .font(.ssBlackTitle2)
+                                    .foregroundColor(.Gray800)
+                            }                        .padding(.horizontal, 10)
+                            Spacer()
+                        }
+                        .padding(.leading, 20)
+                    }
+                    VStack {
+                        Spacer()
+                            .frame(height: 30)
+                        HStack(spacing: 30) {
+                            Button {
+                                withAnimation {
+                                    isProgress = true
+                                }
+                            } label: {
+                                Text("진행 중(\(progressNum))")
+                                    .font(.ssWhiteBody1)
+                                    .foregroundColor(isProgress ? .Gray900 : .Gray500)
                             }
-                        } else {
-                            HStack {
-                                Spacer()
-                                    .frame(width: 165)
-                                Rectangle()
-                                    .frame(width: 100, height: 2)
-                                    .foregroundColor(.GrayBlack)
+                            Spacer()
+                                .frame(width: 50)
+                            Button {
+                                withAnimation {
+                                    isProgress = false
+                                }
+                            } label: {
+                                Text("완료(\(completeNum))")
+                                    .font(.ssWhiteBody1)
+                                    .foregroundColor(isProgress ? .Gray500 : .Gray900)
                             }
                         }
-                    }
-                    if isProgress {
-                        ProgressProjectView()
-                    } else {
-                        CompleteProjectView()
+                        ZStack {
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.Gray300)
+                            if isProgress {
+                                HStack {
+                                    Rectangle()
+                                        .frame(width: 100, height: 2)
+                                        .foregroundColor(.GrayBlack)
+                                    Spacer()
+                                        .frame(width: 165)
+                                }
+                            } else {
+                                HStack {
+                                    Spacer()
+                                        .frame(width: 165)
+                                    Rectangle()
+                                        .frame(width: 100, height: 2)
+                                        .foregroundColor(.GrayBlack)
+                                }
+                            }
+                        }
+                        if isProgress {
+                            ProgressProjectView()
+                        } else {
+                            CompleteProjectView()
+                        }
                     }
                 }
+                .background(Color.Gray200)
             }
         }
     }
--- SeeSaw/Main/Value/ValueView.swift
+++ SeeSaw/Main/Value/ValueView.swift
@@ -11,63 +11,63 @@ struct ValueView: View {
     var valuesExistingYear: [String] = ["2023", "2022", "2021", "2020"]
     @State var valueYear: Int = 0
     var userName: String = "에몽"
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            HStack {
-                Image("SeeSawLogoImage")
-                Spacer()
-                Button {
+        NavigationView {
+            VStack(spacing: 0) {
+                MainToolBar(feature: .valueLog)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    VStack(alignment: .leading) {
+                        Text("가치 설계도")
+                            .font(.ssHeading1)
+                        Text("\(userName)님의 일과 삶의 균형을 위해")
+                            .fontWeight(.medium)
+                            .font(.system(size: 14))
+                            .foregroundColor(.Gray600)
+                        Text("일상과 프로젝트에서 이 가치를 찾아보아요")
+                            .fontWeight(.medium)
+                            .font(.system(size: 14))
+                            .foregroundColor(.Gray600)
+                        GifImage("ValueImage")
+                            .frame(width: 350, height: 185)
+                    }
                     
-                } label: {
-                    Image(systemName: "person.crop.circle")
-                        .resizable()
-                        .frame(width: 28, height: 28)
-                        .foregroundColor(.Gray400)
-                }
-            }
-            VStack(alignment: .leading) {
-                Text("가치 설계도")
-                    .font(.ssHeading1)
-                Text("\(userName)님의 일과 삶의 균형을 위해")
-                    .fontWeight(.medium)
-                    .font(.system(size: 14))
-                    .foregroundColor(.Gray600)
-                Text("일상과 프로젝트에서 이 가치를 찾아보아요")
-                    .fontWeight(.medium)
-                    .font(.system(size: 14))
-                    .foregroundColor(.Gray600)
-                GifImage("ValueImage")
-                    .frame(width: 350, height: 185)
-            }
-            Rectangle()
-                .foregroundColor(.green)
-                .frame(width: 80, height: 5)
-            Text("에몽님의")
-                .font(.ssHeading1)
-            Text(valuesExistingYear[valueYear] + "년의 가치는")
-                .font(.ssHeading1)
-            ValueYearView(valueYear: self.valueYear)
-            ScrollView(.horizontal) {
-                HStack {
-                    ForEach(valuesExistingYear, id: \.self, content: {(dataItem: String) in
-                        Button {
-                            valueYear = valuesExistingYear.firstIndex(of: dataItem) ?? 0
-                        } label: {
-                            Text("\(dataItem)")
-                                .foregroundColor(valuesExistingYear[valueYear] == dataItem ? .white : .Gray400)
-                                .padding(.horizontal, 12)
-                                .padding(.vertical, 4)
-                                .background(valuesExistingYear[valueYear] == dataItem ? Color.SeeSawGreen : Color.Gray300)
-                                .cornerRadius(30)
+                    Rectangle()
+                        .foregroundColor(.green)
+                        .frame(width: 80, height: 5)
+                    
+                    Text("에몽님의")
+                        .font(.ssHeading1)
+                    Text(valuesExistingYear[valueYear] + "년의 가치는")
+                        .font(.ssHeading1)
+                    ValueYearView(valueYear: self.valueYear)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(valuesExistingYear, id: \.self, content: {(dataItem: String) in
+                                Button {
+                                    valueYear = valuesExistingYear.firstIndex(of: dataItem) ?? 0
+                                } label: {
+                                    Text("\(dataItem)")
+                                        .foregroundColor(valuesExistingYear[valueYear] == dataItem ? .white : .Gray400)
+                                        .padding(.horizontal, 12)
+                                        .padding(.vertical, 4)
+                                        .background(valuesExistingYear[valueYear] == dataItem ? Color.SeeSawGreen : Color.Gray300)
+                                        .cornerRadius(30)
+                                }
+                                
+                            })
                         }
-                        
-                    })
+                    }
+                    
+                    Spacer()
                 }
+                .padding(20)
+                .background(Color.Gray200)
             }
-            Spacer()
+            .navigationTitle("")
         }
-        .padding(20)
-        .background(Color.Gray200)
     }
 }
 
