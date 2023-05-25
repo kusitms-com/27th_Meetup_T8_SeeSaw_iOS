4185d395d51cc6ca93b80bbf48c1fb057a512594
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-25 18:22:23 +0900

    Feat(project-report): 두 번째 리포트 api 구현

--- SeeSaw/Main/Project/ProjectReport/ProjectReportViewModel.swift
+++ SeeSaw/Main/Project/ProjectReport/ProjectReportViewModel.swift
@@ -13,10 +13,10 @@ struct GetProjectReportFirstResponse: Codable {
     let isSuccess: Bool
     let code: Int
     let message: String
-    let result: ProjectReportResult
+    let result: ProjectReportFirstResult
 }
 
-struct ProjectReportResult: Codable {
+struct ProjectReportFirstResult: Codable {
     let projectReportInfoDto: ProjectReportInfo
     let emotion1: String
     let emotion2: String
@@ -57,7 +57,7 @@ class ProjectReportViewModel: ObservableObject {
     let keychain = KeychainSwift()
     let baseUrl = "http://\(Bundle.main.infoDictionary?["BASE_URL"] ?? "nil baseUrl")"
     
-    func getProjectReportFirst(projectId: Int, completion: @escaping (ProjectReportResult) -> Void) {
+    func getProjectReportFirst(projectId: Int, completion: @escaping (ProjectReportFirstResult) -> Void) {
         let url = "\(baseUrl)/api/project/\(projectId)/report"
         let headers: HTTPHeaders = [
             "Authorization": "Bearer \(keychain.get("accessToken") ?? "")"
@@ -73,4 +73,54 @@ class ProjectReportViewModel: ObservableObject {
                 }
             }
     }
+    
+    func getProjectReportSecond(projectId: Int, completion: @escaping (ProjectReportSecondResult) -> Void) {
+        let url = "\(baseUrl)/api/project/{projectId}/report/battery"
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(keychain.get("accessToken") ?? "")"
+        ]
+        
+        AF.request(url, method: .get, encoding: JSONEncoding.default, headers: headers)
+            .responseDecodable(of: GetProjectReportSecondResponse.self) { response in
+                switch response.result {
+                case .success(let res):
+                    completion(res.result)
+                case .failure(let error):
+                    print("DEBUG get project report 2: \(error)")
+                }
+            }
+    }
+}
+
+struct GetProjectReportSecondResponse: Codable {
+    let isSuccess: Bool
+    let code: Int
+    let message: String
+    let result: ProjectReportSecondResult
+}
+
+struct ProjectReportSecondResult: Codable {
+    let energyReport: EnergyReport
+    let chargeReport: ChargeReport
+    let healthReport: HealthReport
+}
+
+struct EnergyReport: Codable {
+    let avg: Int
+    let max: Int
+    let min: Int
+}
+
+struct ChargeReport: Codable {
+    let valueCountList: [ValueCount]
+}
+
+struct ValueCount: Codable {
+    let name: String
+    let count: Int
+}
+
+struct HealthReport: Codable {
+    let activityAvg: Int
+    let sleepAvg: Int
 }
